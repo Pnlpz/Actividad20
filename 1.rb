d76daf329a49699d0b3510a65e020adc095c4f90
Marisol Smith <marisolsmith@gmail.com>
2018-05-25 19:44:24 -0400

    ejercicio 1

--- 1.rb
+++ 1.rb
@@ -1,4 +1,5 @@
 class Alumno
+
  def initialize(nombre, nota1, nota2, nota3, nota4)
  @nombre = nombre
  @nota1 = nota1
@@ -6,13 +7,16 @@ class Alumno
  @nota3 = nota3
  @nota4 = nota4
  end
+
+ def self.read_file(filename='notas.txt')
+   alumnos = []
+   data = []
+   File.open(filename, 'r') { |file| data = file.readlines }
+   data.each do |alumno|
+   alumnos << Alumno.new(*alumno.split(', '))
+   end
+   puts alumnos
+ end
 end
-alumnos = []
-Actividad 020 - Módulos, Mixins y Rack.
-Ejercicio 1: Método de clase
-data = []
-File.open('notas.txt', 'r') { |file| data = file.readlines }
-data.each do |alumno|
- alumnos << Alumno.new(*alumno.split(', '))
-end
-print alumnos
+
+Alumno.read_file
